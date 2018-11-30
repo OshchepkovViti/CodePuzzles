e74a61df3a1925a53cc5a8263d975cbff5635d6e
Viktor <viti1135@yandex.ru>
2018-11-30 13:46:36 +0300

    Start

---
+++ CodePuzzle/ConectData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace CodePuzzle
+{
+    class ConectData
+    {
+        public static SqlConnection conn = new SqlConnection(@" Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\data_code.mdf;Integrated Security=True");
+    }
+}
